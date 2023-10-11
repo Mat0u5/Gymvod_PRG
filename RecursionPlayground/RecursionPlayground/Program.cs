0c1ee7204e9fc38760d5ca40df99d39e892259b6
Mat0u5 <matous.danko+github@gmail.com>
2023-10-11 11:05:31 +0200

    RecursionPlayground finished2

--- RecursionPlayground/RecursionPlayground/Program.cs
+++ RecursionPlayground/RecursionPlayground/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -20,21 +21,36 @@ namespace RecursionPlayground
             int factorial = Factorial(n); // Prvni zavolani pro vypocet faktorialu, ulozeni do promenne factorial.
             int fibonacci = Fibonacci(n); // Prvni zavolani pro vypocet Fibonacciho posloupnosti, ulozeni do promenne fibonacci.
             Console.WriteLine($"Pro cislo {n} je faktorial {factorial} a {n}. prvek Fibonacciho posloupnosti je {fibonacci}"); // Vypsani vysledku uzivateli.
+            String fileSearch = findFile("C:\\Users\\matou\\Desktop", "testFile.txt");
+            Console.WriteLine("\ntest: " + fileSearch);
             Console.ReadKey();
         }
 
+        static string findFile(string startingFolderPath, string fileName)
+        {
+            string[] subfolders = Directory.GetDirectories(startingFolderPath);
+            string[] subfiles = Directory.GetFiles(startingFolderPath);
+            foreach (string subfolder in subfolders)
+            {
+                string newReturn = findFile(subfolder, fileName);
+                if (newReturn != "") return newReturn;
+            }
+            foreach (string subfile in subfiles)
+            {
+                if (subfile.EndsWith("\\" + fileName)) return subfile;
+            }
+            return "";
+        }
         static int Factorial(int n)
         {
-            int result = 1;
-            for (int i = n; i > 0; i--) result *= i;
-            return result;
+            if (n != 1) return n*Factorial(n-1);
+            return 1;
         }
 
         static int Fibonacci(int n)
         {
-            List<int> ints = new List<int>() {0,1};
-            while(ints.Count <= n) ints.Add(ints[ints.Count - 1] + ints[ints.Count - 2]);
-            return (n > 0) ? ints[n] : -1;
+            if (n == 0 || n == 1) return (n==0) ? 0 : 1;
+            return Fibonacci(n-1)+ Fibonacci(n - 2);
         }
     }
 }
