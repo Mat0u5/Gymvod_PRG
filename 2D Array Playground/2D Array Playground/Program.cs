21aac29ba94bbe1833174581ad467152c5cd588f
Mat0u5 <matous.danko+github@gmail.com>
2023-11-02 19:24:02 +0100

    Refactored, output changes colored

--- 2D Array Playground/2D Array Playground/Program.cs
+++ 2D Array Playground/2D Array Playground/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Data.Common;
 
 /*
  * Made by Jan Borecky for PRG seminar at Gymnazium Voderadska, year 2023-2024.
@@ -13,94 +16,162 @@ namespace _2D_Array_Playground
 {
     internal class Program
     {
+        static int[,] lastArray = new int[0,0];
         static void Main(string[] args)
         {
             //TODO 1: Vytvoř integerové 2D pole velikosti 5 x 5, naplň ho čísly od 1 do 25 a vypiš ho do konzole (5 řádků po 5 číslech).
-            int[][] array = new int[5][] {new int[5], new int[5], new int[5], new int[5], new int[5]};
-            int column = 0;
-            for (int i = 1; i <= 25; i++) 
-            {
-                if ((i-1) % 5 == 0 && i != 1) column++;
-                int row = i - 1 - column * 5;
-                array[column][row] = i;
-            }
-
+            int[ , ] array = createAndFillArray(2,5);
+            lastArray = (int[,]) array.Clone();
             //TODO 2: Vypiš do konzole n-tý řádek pole, kde n určuje proměnná nRow.
-            int nRow = 0;
-            Console.WriteLine(String.Join(", ", array[nRow]));
-
+            printArrayRow(array, 0);
             //TODO 3: Vypiš do konzole n-tý sloupec pole, kde n určuje proměnná nColumn.
-            int nColumn = 0;
-            for (int i = 0; i < 5; i++)
+            printArrayColumn(array, 0);
+            //TODO 4: Prohoď prvek na souřadnicích [xFirst, yFirst] s prvkem na souřadnicích [xSecond, ySecond] a vypiš celé pole do konzole po prohození.
+            array = swapTwoElements(array, new int[]{0,1}, new int[] {1,1});
+            printArray(array, true);
+            //TODO 5: Prohoď n-tý řádek v poli s m-tým řádkem (n je dáno proměnnou nRowSwap, m mRowSwap) a vypiš celé pole do konzole po prohození.
+            array = swapRows(array,0,1);
+            printArray(array, true);
+            //TODO 6: Prohoď n-tý sloupec v poli s m-tým sloupcem (n je dáno proměnnou nColSwap, m mColSwap) a vypiš celé pole do konzole po prohození.
+            array = swapColumns(array, 0, 1);
+            printArray(array, true);
+            //TODO 7: Otoč pořadí prvků na hlavní diagonále (z levého horního rohu do pravého dolního rohu) a vypiš celé pole do konzole po otočení.
+            //array = transposeMain(array);
+            //printArray(array, true);
+
+            //TODO 8: Otoč pořadí prvků na vedlejší diagonále (z pravého horního rohu do levého dolního rohu) a vypiš celé pole do konzole po otočení.
+            //array = transposeSecondary(array);
+            //printArray(array, true);
+
+            Console.ReadKey();
+        }
+        static int[,] createAndFillArray(int width, int height)
+        {
+            int[,] array = new int[height, width];
+            int column = 0;
+            for (int i = 1; i <= width * height; i++)
             {
-                Console.Write(array[i][nColumn]);
-                if (i != 4) Console.Write(", ");
+                if ((i - 1) % width == 0 && i != 1) column++;
+                int row = i - 1 - column * width;
+                array[column, row] = i;
+            }
+            return array;
+        }
+        static void WriteColorLine(string value, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+        static void WriteColor(string value, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(value);
+            Console.ResetColor();
+        }
+        static void printArray(int[,] array, bool showChanges)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write("    ");
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    String addToEnd = (j != array.GetLength(1) - 1) ? ", " : "";
+                    String printElement = Convert.ToString(array[i, j]) + addToEnd;
+                    try
+                    {
+                        //if the elements from the lastArray (array before last change) dont match, print with red color
+                        if (array[i, j] == lastArray[i, j]) Console.Write(printElement);
+                        else WriteColor(printElement, ConsoleColor.Red);
+                    }
+                    catch (Exception)
+                    {
+                        //lastArray and array have a different size
+                        WriteColor(printElement, ConsoleColor.Red);
+                    }
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+            lastArray = (int[,])array.Clone();
+        }
+        static void printArrayRow(int[,] array, int row)
+        {
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                Console.Write(array[row, i]);
+                if (i != array.GetLength(1) - 1) Console.Write(", ");
                 else Console.Write("\n\n");
             }
-
-            //TODO 4: Prohoď prvek na souřadnicích [xFirst, yFirst] s prvkem na souřadnicích [xSecond, ySecond] a vypiš celé pole do konzole po prohození.
-            //Nápověda: Budeš potřebovat proměnnou navíc, do které si uložíš první z prvků před tím, než ho přepíšeš druhým, abys hodnotou prvního prvku potom mohl přepsat druhý
-            int xFirst = 0, yFirst= 1, xSecond = 4, ySecond = 4;
-            int temp = array[yFirst][xFirst];
-            array[yFirst][xFirst] = array[ySecond][xSecond];
-            array[ySecond][xSecond] = temp;
-            for (int i = 0; i <5; i++)  Console.WriteLine(String.Join(", ", array[i]));
-            Console.WriteLine("");
-            //TODO 5: Prohoď n-tý řádek v poli s m-tým řádkem (n je dáno proměnnou nRowSwap, m mRowSwap) a vypiš celé pole do konzole po prohození.
-            int nRowSwap = 0;
-            int mRowSwap = 1;
-            int[] tempRowSwap = array[nRowSwap];
-            array[nRowSwap] = array[mRowSwap];
-            array[mRowSwap] = tempRowSwap;
-            for (int i = 0; i < 5; i++) Console.WriteLine(String.Join(", ", array[i]));
-            Console.WriteLine("");
-
-            //TODO 6: Prohoď n-tý sloupec v poli s m-tým sloupcem (n je dáno proměnnou nColSwap, m mColSwap) a vypiš celé pole do konzole po prohození.
-            int nColSwap = 0;
-            int mColSwap = 1;
-            for (int i = 0; i < 5; i++)
+        }
+        static void printArrayColumn(int[,] array, int col)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                int tempColSwap = array[i][nColSwap];
-                array[i][nColSwap] = array[i][mColSwap];
-                array[i][mColSwap] = tempColSwap;
+                Console.Write(array[i, col]);
+                if (i != array.GetLength(0) - 1) Console.Write(", ");
+                else Console.Write("\n\n");
             }
-            for (int i = 0; i < 5; i++) Console.WriteLine(String.Join(", ", array[i]));
-            Console.WriteLine("");
-
-            //TODO 7: Otoč pořadí prvků na hlavní diagonále (z levého horního rohu do pravého dolního rohu) a vypiš celé pole do konzole po otočení.
-
+        }
+        static int[,] transposeMain(int[,] array)
+        {
             for (int col = 0; col < 5; col++)
             {
                 for (int row = 0; row < 5; row++)
                 {
                     if (col <= row) continue;
-                    int tempNum = array[col][row];
-                    array[col][row] = array[row][col];
-                    array[row][col] = tempNum;
+                    int temp = array[col, row];
+                    array[col, row] = array[row,col];
+                    array[row, col] = temp;
                 }
             }
-            for (int i = 0; i < 5; i++) Console.WriteLine(String.Join(", ", array[i]));
-            Console.WriteLine("");
-            //TODO 8: Otoč pořadí prvků na vedlejší diagonále (z pravého horního rohu do levého dolního rohu) a vypiš celé pole do konzole po otočení.
-            List<int> list = new List<int>() {0,1,2,3};
-            int swapRow = 0;
+            return array;
+        }
+        static int[,] transposeSecondary(int[,] array)
+        {
+            List<int> list = new List<int>() { 0, 1, 2, 3 };
+            int row = 0;
             while (list.Count > 0)
             {
-                foreach (int swapColumn in list)
+                foreach (int col in list)
                 {
-                    int maxIndex = (swapColumn > swapRow) ? swapColumn : swapRow;
+                    int maxIndex = (col > row) ? col : row;
                     int moveBy = 4 - maxIndex;
-                    int tempMove = array[swapRow][swapColumn];
-                    array[swapRow][swapColumn] = array[swapRow+ moveBy][swapColumn+ moveBy];
-                    array[swapRow + moveBy][swapColumn + moveBy] = tempMove;
+                    int tempMove = array[row, col];
+                    array[row, col] = array[row + moveBy, col + moveBy];
+                    array[row + moveBy, col + moveBy] = tempMove;
                 }
-                list.RemoveAt(list.Count-1);
-                swapRow++;
+                list.RemoveAt(list.Count - 1);
+                row++;
             }
-            for (int i = 0; i < 5; i++) Console.WriteLine(String.Join(", ", array[i]));
-            Console.WriteLine("");
-
-            Console.ReadKey();
+            return array;
+        }
+        static int[,] swapRows(int[,] array, int swapAt, int swapWith)
+        {
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                int temp = array[swapAt,i];
+                array[swapAt,i] = array[swapWith,i];
+                array[swapWith,i] = temp;
+            }
+            return array;
+        }
+        static int[,] swapColumns(int[,] array, int swapAt, int swapWith)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int temp = array[i, swapAt];
+                array[i, swapAt] = array[i, swapWith];
+                array[i, swapWith] = temp;
+            }
+            return array;
+        }
+        static int[,] swapTwoElements(int[,] array, int[] swapAt, int[] swapWith)
+        {
+            int temp = array[swapAt[1],swapAt[0]];
+            array[swapAt[1],swapAt[0]] = array[swapWith[1],swapWith[0]];
+            array[swapWith[1],swapWith[0]] = temp;
+            return array;
         }
     }
 }
