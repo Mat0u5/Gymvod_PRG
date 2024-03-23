611d78aabb3c15f377fc3ae94f8d9ea8a9be1591
Mat0u5 <matous.danko+github@gmail.com>
2024-03-23 09:28:35 +0100

    HW progress

---
+++ FormsPaint/FormsPaint/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace FormsPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paintBrushBox = new System.Windows.Forms.PictureBox();
+            this.paintPanel = new System.Windows.Forms.Panel();
+            this.testLabel = new System.Windows.Forms.Label();
+            this.thicknessSlider = new System.Windows.Forms.TrackBar();
+            this.thicknessLabel = new System.Windows.Forms.Label();
+            this.thicknessTextBox = new System.Windows.Forms.TextBox();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.currentColorBox = new System.Windows.Forms.PictureBox();
+            this.undoBox = new System.Windows.Forms.PictureBox();
+            this.clearCanvasBox = new System.Windows.Forms.PictureBox();
+            this.saveFileBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.paintBrushBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thicknessSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currentColorBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.undoBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clearCanvasBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saveFileBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paintBrushBox
+            // 
+            this.paintBrushBox.Image = global::FormsPaint.Properties.Resources.icons8_paint_100;
+            this.paintBrushBox.Location = new System.Drawing.Point(921, 101);
+            this.paintBrushBox.Name = "paintBrushBox";
+            this.paintBrushBox.Size = new System.Drawing.Size(45, 44);
+            this.paintBrushBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.paintBrushBox.TabIndex = 0;
+            this.paintBrushBox.TabStop = false;
+            this.paintBrushBox.Click += new System.EventHandler(this.pictureBoxClickEvent);
+            // 
+            // paintPanel
+            // 
+            this.paintPanel.BackColor = System.Drawing.Color.White;
+            this.paintPanel.Location = new System.Drawing.Point(69, 87);
+            this.paintPanel.Name = "paintPanel";
+            this.paintPanel.Size = new System.Drawing.Size(633, 432);
+            this.paintPanel.TabIndex = 1;
+            this.paintPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelMouseDrawEvent);
+            this.paintPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelMouseDrawEvent);
+            this.paintPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelMouseDrawEvent);
+            // 
+            // testLabel
+            // 
+            this.testLabel.AutoSize = true;
+            this.testLabel.Location = new System.Drawing.Point(144, 48);
+            this.testLabel.Name = "testLabel";
+            this.testLabel.Size = new System.Drawing.Size(35, 13);
+            this.testLabel.TabIndex = 2;
+            this.testLabel.Text = "label1";
+            // 
+            // thicknessSlider
+            // 
+            this.thicknessSlider.Cursor = System.Windows.Forms.Cursors.No;
+            this.thicknessSlider.Location = new System.Drawing.Point(718, 326);
+            this.thicknessSlider.Maximum = 50;
+            this.thicknessSlider.Minimum = 1;
+            this.thicknessSlider.Name = "thicknessSlider";
+            this.thicknessSlider.Size = new System.Drawing.Size(171, 45);
+            this.thicknessSlider.TabIndex = 3;
+            this.thicknessSlider.Value = 1;
+            this.thicknessSlider.ValueChanged += new System.EventHandler(this.sliderEvent);
+            // 
+            // thicknessLabel
+            // 
+            this.thicknessLabel.AutoSize = true;
+            this.thicknessLabel.Location = new System.Drawing.Point(715, 302);
+            this.thicknessLabel.Name = "thicknessLabel";
+            this.thicknessLabel.Size = new System.Drawing.Size(56, 13);
+            this.thicknessLabel.TabIndex = 4;
+            this.thicknessLabel.Text = "Thickness";
+            // 
+            // thicknessTextBox
+            // 
+            this.thicknessTextBox.Location = new System.Drawing.Point(810, 300);
+            this.thicknessTextBox.Name = "thicknessTextBox";
+            this.thicknessTextBox.Size = new System.Drawing.Size(68, 20);
+            this.thicknessTextBox.TabIndex = 5;
+            // 
+            // currentColorBox
+            // 
+            this.currentColorBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.currentColorBox.Location = new System.Drawing.Point(810, 406);
+            this.currentColorBox.Name = "currentColorBox";
+            this.currentColorBox.Size = new System.Drawing.Size(64, 60);
+            this.currentColorBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.currentColorBox.TabIndex = 6;
+            this.currentColorBox.TabStop = false;
+            this.currentColorBox.Click += new System.EventHandler(this.pictureBoxClickEvent);
+            // 
+            // undoBox
+            // 
+            this.undoBox.Image = global::FormsPaint.Properties.Resources.icons8_paint_100;
+            this.undoBox.Location = new System.Drawing.Point(784, 114);
+            this.undoBox.Name = "undoBox";
+            this.undoBox.Size = new System.Drawing.Size(45, 44);
+            this.undoBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.undoBox.TabIndex = 7;
+            this.undoBox.TabStop = false;
+            this.undoBox.Click += new System.EventHandler(this.pictureBoxClickEvent);
+            // 
+            // clearCanvasBox
+            // 
+            this.clearCanvasBox.Image = global::FormsPaint.Properties.Resources.icons8_paint_100;
+            this.clearCanvasBox.Location = new System.Drawing.Point(844, 192);
+            this.clearCanvasBox.Name = "clearCanvasBox";
+            this.clearCanvasBox.Size = new System.Drawing.Size(45, 44);
+            this.clearCanvasBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.clearCanvasBox.TabIndex = 8;
+            this.clearCanvasBox.TabStop = false;
+            this.clearCanvasBox.Click += new System.EventHandler(this.pictureBoxClickEvent);
+            // 
+            // saveFileBox
+            // 
+            this.saveFileBox.Image = global::FormsPaint.Properties.Resources.icons8_paint_100;
+            this.saveFileBox.Location = new System.Drawing.Point(921, 231);
+            this.saveFileBox.Name = "saveFileBox";
+            this.saveFileBox.Size = new System.Drawing.Size(45, 44);
+            this.saveFileBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.saveFileBox.TabIndex = 9;
+            this.saveFileBox.TabStop = false;
+            this.saveFileBox.Click += new System.EventHandler(this.pictureBoxClickEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1000, 581);
+            this.Controls.Add(this.saveFileBox);
+            this.Controls.Add(this.clearCanvasBox);
+            this.Controls.Add(this.undoBox);
+            this.Controls.Add(this.currentColorBox);
+            this.Controls.Add(this.thicknessTextBox);
+            this.Controls.Add(this.thicknessLabel);
+            this.Controls.Add(this.thicknessSlider);
+            this.Controls.Add(this.testLabel);
+            this.Controls.Add(this.paintPanel);
+            this.Controls.Add(this.paintBrushBox);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.onLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.paintBrushBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thicknessSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currentColorBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.undoBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clearCanvasBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saveFileBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox paintBrushBox;
+        private System.Windows.Forms.Panel paintPanel;
+        private System.Windows.Forms.Label testLabel;
+        private System.Windows.Forms.TrackBar thicknessSlider;
+        private System.Windows.Forms.Label thicknessLabel;
+        private System.Windows.Forms.TextBox thicknessTextBox;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.PictureBox currentColorBox;
+        private System.Windows.Forms.PictureBox undoBox;
+        private System.Windows.Forms.PictureBox clearCanvasBox;
+        private System.Windows.Forms.PictureBox saveFileBox;
+    }
+}
+
---
+++ FormsPaint/FormsPaint/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FormsPaint
+{
+    public partial class Form1 : Form
+    {
+        Graphics panelGraphics;
+        Graphics bitmapGraphics;
+        float lastMouseX;
+        float lastMouseY;
+        float startStrokeMouseX = float.MinValue;
+        float startStrokeMouseY = float.MinValue;
+        float thickness; 
+        Color selectedColor;
+        Bitmap currentBitmap;
+        List<Bitmap> panelBitmapHistory = new List<Bitmap>();
+        MouseButtons lastMouseMoveButton;
+        string currentPaintMode = "pen";
+        public Form1()
+        {
+            InitializeComponent();
+            /*
+            PictureBox picturebox = new PictureBox();
+            picturebox.Name = Convert.ToString(square);
+            picturebox.BackColor = squareColor;
+            picturebox.Size = new Size(squareSize, squareSize);
+            picturebox.Location = new Point(x, y);
+            picturebox.SizeMode = PictureBoxSizeMode.StretchImage;
+            picturebox.Click += (sender, e) => {
+                onClick(Convert.ToInt32(picturebox.Name));
+            };
+            if (piece == "br") picturebox.Image = Properties.Resources.br;*/
+        }
+
+
+        private void onLoad(object sender, EventArgs e)
+        {
+            currentBitmap = new Bitmap(paintPanel.Width, paintPanel.Height);
+            bitmapGraphics = Graphics.FromImage(currentBitmap);
+            panelGraphics = paintPanel.CreateGraphics();
+            saveBitmapToList();
+            changeThickness(15);
+            changeColor(Color.Blue);
+            panelGraphics.Clear(Color.White);
+            bitmapGraphics.Clear(Color.White);
+        }
+        private void pictureBoxClickEvent(object sender, EventArgs e)
+        {
+            PictureBox picturebox = (PictureBox)sender;
+            testLabel.Text = picturebox.Name;
+            if (picturebox.Equals(paintBrushBox))
+            {
+                if (currentPaintMode == "pen") currentPaintMode = "circle";
+                else if (currentPaintMode == "circle") currentPaintMode = "pen";
+            }
+            if (picturebox.Equals(saveFileBox))
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.FileName = "formsPaintSave";
+                dialog.DefaultExt = "bmp";
+                dialog.ValidateNames = true;
+                dialog.Filter = "Bitmap Image (.bmp)|*.bmp|JPEG Image (.jpeg)|*.jpeg|Png Image (.png)|*.png|Tiff Image (.tiff)|*.tiff|Wmf Image (.wmf)|*.wmf";
+
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (dialog.FileName.EndsWith(".bmp")) currentBitmap.Save(dialog.FileName, ImageFormat.Bmp);
+                    if (dialog.FileName.EndsWith(".jpeg")) currentBitmap.Save(dialog.FileName, ImageFormat.Jpeg);
+                    if (dialog.FileName.EndsWith(".png")) currentBitmap.Save(dialog.FileName, ImageFormat.Png);
+                    if (dialog.FileName.EndsWith(".tiff")) currentBitmap.Save(dialog.FileName, ImageFormat.Tiff);
+                    if (dialog.FileName.EndsWith(".wmf")) currentBitmap.Save(dialog.FileName, ImageFormat.Wmf);
+                }
+            }
+            if (picturebox.Equals(clearCanvasBox))
+            {
+                panelGraphics.Clear(Color.White);
+                bitmapGraphics.Clear(Color.White);
+                panelBitmapHistory = new List<Bitmap>();
+                saveBitmapToList();
+            }
+            if (picturebox.Equals(undoBox))
+            {
+                restoreBitmapToPanel(getLastBitmap(true));
+            }
+            if (picturebox.Equals(currentColorBox))
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    changeColor(colorDialog.Color);
+                }
+            }
+        }
+
+        private void saveBitmapToList()
+        {
+            Bitmap bitmap = new Bitmap(currentBitmap);
+            panelBitmapHistory.Add(bitmap);
+        }
+        private void restoreBitmapToPanel(Bitmap bmap)
+        {
+            if (bmap == null) return;
+            panelGraphics.Clear(Color.White);
+            panelGraphics.DrawImage(bmap, new Point(0, 0));
+            currentBitmap = new Bitmap(bmap);
+            bitmapGraphics = Graphics.FromImage(currentBitmap);
+
+        }
+        private Bitmap getLastBitmap(bool deleteAccessedMap)
+        {
+            if (panelBitmapHistory.Count < 2) return null;
+            Bitmap lastBitmap = panelBitmapHistory[panelBitmapHistory.Count - 2];
+            if (deleteAccessedMap) panelBitmapHistory.RemoveAt(panelBitmapHistory.Count - 1);
+            Console.WriteLine("Maps Saved: " + panelBitmapHistory.Count);
+            return lastBitmap;
+}
+        private void panelMouseDrawEvent(object sender, MouseEventArgs e)
+        {
+            float mouseX = e.Location.X;
+            float mouseY = e.Location.Y;
+            if (e.Button == MouseButtons.Left)
+            {
+                if (startStrokeMouseX == float.MinValue && startStrokeMouseY == float.MinValue)
+                {
+
+                    startStrokeMouseX = mouseX;
+                    startStrokeMouseY = mouseY;
+                }
+
+                if (currentPaintMode == "pen") drawLine(mouseX, mouseY, "lineEllipse");
+                if (currentPaintMode == "circle")
+                {
+                    drawCircle(mouseX, mouseY, true);
+                }
+            }
+            if (e.Button != MouseButtons.Left && lastMouseMoveButton == MouseButtons.Left)
+            {
+                if (currentPaintMode == "circle") drawCircle(mouseX, mouseY, false);
+                Console.WriteLine("adding...");
+                saveBitmapToList();
+                lastMouseX = -1;
+                lastMouseY = -1;
+            }
+            lastMouseMoveButton = e.Button;
+            lastMouseX = mouseX;
+            lastMouseY = mouseY;
+        }
+        private void drawCircle(float mouseX, float mouseY, bool isShadow)
+        {
+            float radius = Math.Min(mouseX - startStrokeMouseX, mouseY - startStrokeMouseY);
+            
+            if (isShadow)
+            {
+                restoreBitmapToPanel(getLastBitmap(false));
+                Pen pen = new Pen(Color.Gray, 2);
+                panelGraphics.DrawEllipse(pen, startStrokeMouseX - radius, startStrokeMouseY - radius, radius * 2, radius * 2);
+            }
+            else
+            {
+                restoreBitmapToPanel(getLastBitmap(false));
+                Pen pen = new Pen(selectedColor, thickness);
+                panelGraphics.DrawEllipse(pen, startStrokeMouseX - radius, startStrokeMouseY - radius, radius * 2, radius * 2);
+                bitmapGraphics.DrawEllipse(pen, startStrokeMouseX - radius, startStrokeMouseY - radius, radius * 2, radius * 2);
+            }
+           
+        }
+        private void drawLine(float mouseX, float mouseY, String drawType)
+        {
+            Pen pen = new Pen(selectedColor, thickness);
+            SolidBrush brush = new SolidBrush(selectedColor);
+            if (drawType == "lineEllipse") lineEllipse();
+            if (drawType == "customLineEllipse") customLineEllipse();
+            if (drawType == "onlyEllipse") onlyEllipse();
+            void lineEllipse()
+            {
+                panelGraphics.DrawLine(pen, lastMouseX, lastMouseY, mouseX, mouseY);
+                panelGraphics.FillEllipse(brush, mouseX - thickness / 2, mouseY - thickness / 2, thickness, thickness);
+                bitmapGraphics.DrawLine(pen, lastMouseX, lastMouseY, mouseX, mouseY);
+                bitmapGraphics.FillEllipse(brush, mouseX - thickness / 2, mouseY - thickness / 2, thickness, thickness);
+            }
+            void customLineEllipse()
+            {
+                float[] vector = { lastMouseX - mouseX, lastMouseY - mouseY };
+                float[] perpendicularVector = { -vector[1], vector[0] };
+                float vectorSize = getVectorSize(vector);
+                float perpendicularVectorSize = getVectorSize(perpendicularVector);
+                if (vectorSize == 0)
+                {
+                    panelGraphics.FillEllipse(brush, mouseX - thickness / 2, mouseY - thickness / 2, thickness, thickness);
+                    return;
+                }
+                float multiplyVectorBy = (thickness / 2) / vectorSize;
+                perpendicularVector[0] *= multiplyVectorBy;
+                perpendicularVector[1] *= multiplyVectorBy;
+                Point[] points = {new Point(Convert.ToInt32(lastMouseX + perpendicularVector[0]), Convert.ToInt32(lastMouseY + perpendicularVector[1]))
+                        , new Point(Convert.ToInt32(lastMouseX - perpendicularVector[0]),Convert.ToInt32(lastMouseY - perpendicularVector[1]))
+                        , new Point(Convert.ToInt32(mouseX - perpendicularVector[0]), Convert.ToInt32(mouseY - perpendicularVector[1]))
+                        , new Point(Convert.ToInt32(mouseX + perpendicularVector[0]), Convert.ToInt32(mouseY + perpendicularVector[1])) };
+                panelGraphics.FillPolygon(brush, points);
+                GraphicsState lastPanelGraphics = panelGraphics.Save();
+                panelGraphics.Restore(lastPanelGraphics);
+            }
+            void onlyEllipse()
+            {
+                float[] vector = { lastMouseX - mouseX, lastMouseY - mouseY };
+                float vectorSize = getVectorSize(vector);
+                if (vectorSize == 0) vectorSize = 1;
+                float[] stepVector = { vector[0] / vectorSize, vector[1] / vectorSize };
+                for (int i = 0; i < Math.Ceiling(vectorSize); i++)
+                {
+                    float currentX = mouseX + stepVector[0] * i;
+                    float currentY = mouseY + stepVector[1] * i;
+                    panelGraphics.FillEllipse(brush, currentX - thickness / 2, currentY - thickness / 2, thickness, thickness);
+                }
+            }
+        }
+        private float getVectorSize(float[] vector)
+        {
+            return (float)Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2));
+        }
+        private void sliderEvent(object sender, EventArgs e)
+        {
+            System.Windows.Forms.TrackBar slider = (System.Windows.Forms.TrackBar)sender;
+            if (slider.Equals(thicknessSlider))
+            {
+                changeThickness(slider.Value);
+            }
+        }
+
+        private void changeThickness(float newSize)
+        {
+
+            thickness = newSize;
+            //thicknessTimesSqrtTwo = newSize;
+            if (thicknessSlider.Value != newSize) thicknessSlider.Value = (int)newSize;
+            thicknessTextBox.Text = Convert.ToString(newSize);
+        }
+        private void changeColor(Color newColor)
+        {
+            selectedColor = newColor;
+            if (currentColorBox.BackColor != newColor) currentColorBox.BackColor = newColor;
+        }
+    }
+}
