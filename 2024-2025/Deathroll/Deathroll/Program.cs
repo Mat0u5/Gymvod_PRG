f181631cc9815432ed2c5b7fee32e2874b21ed5d
Mat0u5 <matous.danko+github@gmail.com>
2024-09-20 10:41:32 +0200

    DeathRoll finished

--- 2024-2025/Deathroll/Deathroll/Program.cs
+++ 2024-2025/Deathroll/Deathroll/Program.cs
@@ -15,46 +15,44 @@ namespace Deathroll
     {
         static void Main(string[] args)
         {
-            /*
-             * Jednoduchy program na procviceni podminek a cyklu (lze udelat i rekurzi).
-             * 
-             * Vytvor program, kde bude uzivatel hrat s pocitacem deathroll.
-             * Pravidla deathrollu: Prvni hrac navrhne cislo (puvodne ve wowku pocet goldu, o ktere se hraci vsadi) a "rollne" navrhnute cislo, jinak receno je to stejne,
-             * jako kdyby si hodil kostkou s tolika stenami, jako je navrhnute cislo. Prvnimu hraci "padne" nejake cislo a druhy hrac si "rollne" padnute cislo
-             * (ktere uz je mensi nez to predesleho hrace).
-             * Prohrava ten hrac, kteremu padne 1 jako prvnimu.
-             * Ukazka hry: Hraci se shodnou na cisle 1000. Prvni hrac rollne 1-1000, padne mu 920. Druhy hrac rolluje 1-920, padne mu 235 atd. atd. az jednomu z hracu padne 1
-             * a ten prohrava.
-             * 
-             * Struktura:
-             * 
-             * - nadefinuj promenne, ktere budes potrebovat po celou dobu hry, tedy aktualne rollovane cislo a stav "goldu" uzivatele i pocitace (oba zacinaji treba s 1000 goldu)
-             * 
-             * - uzivatel zada prvotni sazku, ktera musi byt maximalne tolik, kolik ma goldu
-             * 
-             * Opakuj dokud nepadne jednomu z hracu 1:
-             * {
-             *      Pokud je sude kolo:
-             *      {
-             *          - uzivatel zada hodnotu, kterou rolluje
-             *          - kontroluj, ze uzivatel zadal spravnou hodnotu
-             *          - uloz rollnute cislo
-             *          - vypis uzivateli, co rollnul
-             *      }
-             *      Pokud je liche kolo:
-             *      {
-             *          - pocitac rollne nahodne cislo mezi 1 a aktualne rollovanym cislem
-             *          - vypis uzivateli, co rollnul pocitac
-             *      }
-             * }
-             * 
-             * 
-             * - posledni hrajici hrac prohral, protoze mu padla 1 a sazku bere druhy hrac
-             * - vypis uzivateli kdo vyhral a stav goldu uzivatele i pocitace
-             * 
-             * ROZSIRENI:
-             * - umozni uzivateli opakovat deathroll dokud ma nejake goldy
-             */
+            Random rnd = new Random();
+            int gold = 1000;
+            while(gold > 0)
+            {
+                Console.WriteLine("Your balance is " + gold + ".");
+                int bet = -1;
+                while (bet == -1)
+                {
+                    Console.WriteLine("Enter your bet.");
+                    if (!Int32.TryParse(Console.ReadLine(), out int newBet)) continue;
+                    if (newBet > gold) Console.WriteLine("You're too broke for that");
+                    else bet = newBet;
+                }
+                int round = 0;
+                int newRoll = bet;
+                while (newRoll > 1)
+                {
+                    round++;
+                    newRoll = rnd.Next(1, newRoll);
+                    if (round % 2 != 0)
+                    {
+                        Console.WriteLine("\nPress any key to roll.");
+                        Console.ReadKey();
+                        Console.WriteLine("You rolled " + newRoll);
+                        if (newRoll > 1) continue;
+                        Console.WriteLine("You lost " + bet + " gold.");
+                        gold -= bet;
+                        break;
+                    }
+
+                    Console.WriteLine("The computer rolled " + newRoll);
+                    if (newRoll > 1) continue;
+                    Console.WriteLine("You win " + bet + " gold.");
+                    gold += bet;
+                }
+            }
+            Console.WriteLine("Hate to break it to yout but uhhh..... You don't have anything left. See ya later.");
+            Console.ReadKey();
         }
     }
 }
