97475ef824ae83109bb8f9eadd05063b16336322
Mat0u5 <matous.danko+github@gmail.com>
2024-10-11 09:58:45 +0200

    RecursionPlayground finished

--- 2024-2025/RecursionPlayground/RecursionPlayground/Program.cs
+++ 2024-2025/RecursionPlayground/RecursionPlayground/Program.cs
@@ -4,34 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*
- * Made by Jan Borecky for PRG seminar at Gymnazium Voderadska, year 2023-2024.
- * Extended by students.
- */
-
 namespace RecursionPlayground
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine()); // Nacteme cislo n, pro ktere budeme pocitat jeho faktorial a n-ty prvek Fibonacciho posloupnosti.
-            int factorial = Factorial(n); // Prvni zavolani pro vypocet faktorialu, ulozeni do promenne factorial.
-            int fibonacci = Fibonacci(n); // Prvni zavolani pro vypocet Fibonacciho posloupnosti, ulozeni do promenne fibonacci.
-            Console.WriteLine($"Pro cislo {n} je faktorial {factorial} a {n}. prvek Fibonacciho posloupnosti je {fibonacci}"); // Vypsani vysledku uzivateli.
+            int n = int.Parse(Console.ReadLine());
+            int factorial = Factorial(n);
+            int fibonacci = Fibonacci(n);
+            Console.WriteLine($"Pro cislo {n} je faktorial {factorial} a {n}. prvek Fibonacciho posloupnosti je {fibonacci}");
             Console.ReadKey();
         }
 
         static int Factorial(int n)
         {
-            // TODO: Urci ukoncovaci podminku pro faktorial a zavolej Factorial zevnitr se spravnym parametrem / vypoctem.
-            return 0; // TODO: Uprav, aby Factorial vracel spravnou hodnotu.
+            if (n == 1) return n;
+            return n * Factorial(n - 1);
         }
 
         static int Fibonacci(int n)
         {
-            // TODO: Urci ukoncovaci podminku pro Fibonacciho a zavolej Fibonacci zevnitr se spravnym parametrem / vypoctem.
-            return 0; // TODO: Uprav, aby Fibonacci vracel spravnou hodnotu.
+            if (n ==2) return 1;
+            if (n == 1) return 0;
+            return Fibonacci(n - 1)+Fibonacci(n-2);
         }
     }
 }
