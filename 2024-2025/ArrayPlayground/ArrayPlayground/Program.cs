cdedc998ef3d827b628fe136d6e861c80bfbe5c5
Mat0u5 <matous.danko+github@gmail.com>
2024-10-25 10:02:33 +0200

    ArrayPlayground finished

--- 2024-2025/ArrayPlayground/ArrayPlayground/Program.cs
+++ 2024-2025/ArrayPlayground/ArrayPlayground/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,41 +16,96 @@ namespace ArrayPlayground
     {
         static void Main(string[] args)
         {
-            //TODO 1: Vytvoř integerové pole a naplň ho pěti libovolnými čísly.
-
+            int[] array = {1, 3, 4, 6, 7};
 
             //TODO 2: Vypiš do konzole všechny prvky pole, zkus jak klasický for, kde i využiješ jako index v poli, tak foreach.
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]+", ");
+            }
+            Console.WriteLine("");
+            foreach (int i in array)
+            {
+                Console.Write(i+ ", ");
+            }
+            Console.WriteLine("");
 
 
-            //TODO 3: Spočti sumu všech prvků v poli a vypiš ji uživateli.
-            int sum;
 
-
-            //TODO 4: Spočti průměr prvků v poli a vypiš ho do konzole.
+            int sum = 0;
             int average;
+            int max = array[0];
+            int min = array[0];
+            foreach (int i in array)
+            {
+                sum += i;
+                if (i > max) max = i;
+                if (i < min) min = i;
+            }
+            average = sum / array.Length;
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {average}");
+            Console.WriteLine($"Max: {max}");
+            Console.WriteLine($"Min: {min}");
 
 
-            //TODO 5: Najdi maximum v poli a vypiš ho do konzole.
-            int max;
-
-
-            //TODO 6: Najdi minimum v poli a vypiš ho do konzole.
-            int min;
-
-
-            //TODO 7: Vyhledej v poli číslo, které zadá uživatel, a vypiš index nalezeného prvku do konzole.
             int index;
+            while(true)
+            {
+                Console.WriteLine("Enter a number.");
+                if (Int32.TryParse(Console.ReadLine(), out int newNum))
+                {
+                    if (newNum < 0) continue;
+                    if (newNum >= array.Length) continue;
+                    index = newNum;
+                    break;
+                }
+                continue;
+            }
+            Console.WriteLine($"Number at index {index} is {array[index]}");
 
-            //TODO 8: Přepiš pole na úplně nové tak, že bude obsahovat 100 náhodně vygenerovaných čísel od 0 do 9.
+
+
             Random rng = new Random();
+            int[] newArray = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                newArray[i] = rng.Next(10);
+            }
+            array = newArray;
+            Console.Write("Changed array: ");
+            foreach (int i in array)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine("");
 
-            //TODO 9: Spočítej kolikrát se každé číslo v poli vyskytuje a spočítané četnosti vypiš do konzole.
+
             int[] counts = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int value = array[i];
+                counts[value] += 1;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Array contains the number {i} {counts[i]} times");
+            }
 
-            //TODO 10: Vytvoř druhé pole, do kterého zkopíruješ prvky z prvního pole v opačném pořadí.
 
 
-            //Zkus is dál hrát s polem dle své libosti. Můžeš třeba prohodit dva prvky, ukládat do pole prvky nějaké posloupnosti (a pak si je vyhledávat) nebo cokoliv dalšího tě napadne
+            int[] reversed = new int[array.Length];
+            for ( int i = 0;i < array.Length; i++)
+            {
+                reversed[array.Length - i - 1] = array[i];
+            }
+            Console.Write("Reversed array: ");
+            foreach (int i in array)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine("");
+
 
             Console.ReadKey();
         }
